a33823b602746e366677748e812f6bca8647bd62
clmb00 <paolo.colombo2000@gmail.com>
2022-10-20 15:54:32 +0200

    [ADD] generation grid

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('check');
+const container = document.querySelector('.main_container');
+const numberRows = 10;
+const playButton = document.querySelector('header button');
+
+playButton.addEventListener('click', function(){
+  container.innerHTML = ""
+  const difficulty = document.querySelector('header select').value;
+  console.log(difficulty);
+  createGrid(numberRows);
+  playButton.innerHTML = "New game"
+})
+
+function createGrid(numberRows){
+
+  for(let i = 0; i < Math.pow(numberRows, 2); i++){
+    container.append(createCell(numberRows, i+1));
+  }
+
+}
+
+function createCell(numberRows, index){
+  const box = document.createElement('div');
+  box.classList.add('box');
+  box.style.width = 'calc(100% / ' + numberRows + ')';
+  box.style.height = 'calc(100% / ' + numberRows + ')';
+  box.innerHTML = index;
+  return box;
+}
